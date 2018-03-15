bc76021a80c72e8418af8ea0b9d0d2534890a0f6
vincentvu101 <31523966+vincentvu101@users.noreply.github.com>
2018-03-15 00:32:54 -0700

    Add files via upload
    Final Version. With added comments

---
+++ FileEncryption/magic.py
@@ -0,0 +1,6 @@
+IV_SIZE = 16
+KEY_SIZE = 32
+PADDING_SIZE = 128
+PIC_PATH = "Capture.JPG"
+ENC_EXT = ".encrypt"
+JSON_FILENAME = "EncryptInfo.json"
--- FileEncryption/main.py
+++ FileEncryption/main.py
@@ -1,96 +1,151 @@
 import os
 import base64
 import json
+import magic
 from base64 import b64encode, b64decode
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 
+#Generate an IV of a given size
 def generateIV():
-  IV = os.urandom(16)
+  IV = os.urandom(magic.IV_SIZE)
   return IV
   
+#Generate an IV, encrypt the message using key and IV in AES-CBC
 def Myencrypt(message, key):
-  if (len(key) != 32):
+  #Checks key for the approriate size
+  if (len(key) != magic.KEY_SIZE):
     print("The key has to be 32 bytes")
     return
   
-  #iv
+  #Generates the IV
   IV = generateIV()
   
-  padder = padding.PKCS7(128).padder()
+  #Initialize the padder to pad the ciphertext
+  padder = padding.PKCS7(magic.PADDING_SIZE).padder()
   padded_data = padder.update(message) + padder.finalize()
   backend = default_backend()
   
+  #Combines the AES algorithm with a mode CBC
   cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend = backend)
+  
+  #Initialize the encryptor instance
   encryptor = cipher.encryptor()
+  
+  #Encrypts the padded plaintext
   ct = encryptor.update(padded_data) + encryptor.finalize()
+  
+  print("Message has been encrypted")
   return ct, IV
 
+#Inverse of Myencrypt function
+#ct: Cipher text
+#iv: Initialization Vector
+#Decrypts the encrypted message created by the previous method
 def Mydecrypt(ct, key, iv):
-  pt = Cipher(algorithms.AES(key), modes.CBC(iv), backend = default_backend())
-  decryptor = pt.decryptor()
-  data = decryptor.update(ct) + decryptor.finalize()
-  unpadder = padding.PKCS7(128).unpadder()
-  data = unpadder.update(data) + unpadder.finalize()
-  return data
   
+  #Combines the AES algorithm with a mode CBC
+  decipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend = default_backend())
+  
+  #Instantiates the decryptor
+  decryptor = decipher.decryptor()
+  
+  #Deciphers the encrypted message
+  pt = decryptor.update(ct) + decryptor.finalize()
+  
+  #Get rid of the padding
+  unpadder = padding.PKCS7(magic.PADDING_SIZE).unpadder()
+  pt = unpadder.update(pt) + unpadder.finalize()
+  print("The message has been decrypted")
+  return pt
+
+#Encrypting a file
 def MyfileEncrypt(filepath):
-  key = os.urandom(32)
+  #Generating a key of a given length
+  key = os.urandom(magic.KEY_SIZE)
   
-  output = filepath + ".encrypt"
+  #Encrypted file name
+  output = filepath + magic.ENC_EXT
+  
   #read file in binary
   file = open(filepath, 'rb')
   bdata = file.read()
   bdata = b64encode(bdata)
   file.close()
   
+  #Using the encryption method, encrypt the data in the image
   ct, iv = Myencrypt(bdata, key)
   
+  #Creating the encrypted file
   encFile = open(output, "wb")
   encFile.write(ct)
   encFile.close()
-  return ct, iv, key, '.encrypt'
-  
+  return ct, iv, key, magic.ENC_EXT
+
+#Decrypting the file. Inverse of MyfileEncrypt
+#This method does not use the Json method to decrypt the file
 def MyfileDecrypt(filepath, key, iv):
+  #Read the file and place its content in a variable
   file = open(filepath, 'rb')
   bdata = file.read()
   file.close()
   
-  filepath.replace(extension, '')
+  #Decrypt the encrypted data
   data = Mydecrypt(bdata, key, iv)
   data = b64decode(data)
-  decrypt = open("Hello.JPG", "wb")
+  
+  #Create a file to hold the decrypted data
+  #Use for comparison with the original image
+  decrypt = open("Decrypted.JPG", "wb")
   decrypt.write(data)
   decrypt.close()
 
 #Writing to Json file
 def WriteJson(ct, key, iv, ext):
-  output = open("EncryptInfo.json", 'w')
+  #Creates a json file
+  output = open(magic.JSON_FILENAME, 'w')
+  
+  #Storing the Ciphertext, Key, IV, and Extension in a json file
   data = {"Cipher text": b64encode(ct).decode('utf-8') , "Key": b64encode(key).decode('utf-8'), "IV": b64encode(iv).decode('utf-8'), "Extension": ext}
   json.dump(data, output)
   output.close()
 
-#Decryption with the json file  
+#Decryption with the json file 
+#Correct Way
 def MyfileDecrypt2(filepath, jsonPath):
-  with open(jsonPath) as jsonFile:
-    EncryptData = json.load(jsonFile)
-    ct = b64decode(EncryptData["Cipher text"])
-    key = b64decode(EncryptData["Key"])
-    iv = b64decode(EncryptData["IV"])
-    ext = EncryptData["Extension"]
+  #Open the Json file for access
+  jsonFile = open(jsonPath)
+  
+  #load the file's content onto variables
+  EncryptData = json.load(jsonFile)
+  ct = b64decode(EncryptData["Cipher text"])
+  key = b64decode(EncryptData["Key"])
+  iv = b64decode(EncryptData["IV"])
+  ext = EncryptData["Extension"]
+  
+  #pt: plaintext
+  #Decrypts the Ciphertext
   pt = Mydecrypt(ct, key, iv)
+  
+  #Change the data to its approriate format
   pt = b64decode(pt)
-  decrypt = open("Hello.JPG", "wb")
+  #Create a file to hold the decrypted data
+  #Use for comparison with the original image
+  decrypt = open("Decrypted.JPG", "wb")
   decrypt.write(pt)
   decrypt.close()
-  
-key = os.urandom(32)
+
+#Testing Purposes
+#Generating a key
+key = os.urandom(magic.KEY_SIZE)
+#Set message to test function
 message = b"hello"
 ct, iv = Myencrypt(message, key)
-print(ct)
-print(Mydecrypt(ct, key, iv))
+print("Original Message: ", message)
+print("Cipher text: ", ct)
+print("Decrypted Message: ", Mydecrypt(ct, key, iv))
 
-ct, iv, key, extension = MyfileEncrypt("Capture.JPG")
+ct, iv, key, extension = MyfileEncrypt(magic.PIC_PATH)
 WriteJson(ct, key, iv, extension)
-MyfileDecrypt2("Capture.JPG.encrypt", "EncryptInfo.json")
+MyfileDecrypt2(magic.PIC_PATH + magic.ENC_EXT, magic.JSON_FILENAME)
